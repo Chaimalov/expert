2eed09b17f405c2880ad58bf16d645b9589eb5ab
israelmark1 <israelmark98@gmail.com>
2022-05-08 22:36:00 +0300

    update

---
+++ back/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBTd_9fw4vCPzFI0TcPrDtt3Gyhe11iL4w",
+    authDomain: "expert-57e13.firebaseapp.com",
+    projectId: "expert-57e13",
+    storageBucket: "expert-57e13.appspot.com",
+    messagingSenderId: "747965781429",
+    appId: "1:747965781429:web:b0e80cd41bdadced2eb5fc",
+    measurementId: "G-KV0YWQVCD2"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebase);
+const db = getFirestore(firebase);
+
+export  { collection, getDocs, db }
+
+
+
+
+
--- back/server.js
+++ back/server.js
@@ -1,34 +1,10 @@
-const express = require("express");
-const app = express();
+import express from "express";
+const app = express()
 const PORT = 8080;
+import {collection, getDocs ,db} from "./firebase.js";
 
+async function getProducts() {
 
-
-// Import the functions you need from the SDKs you need
-const { initializeApp } = require("firebase/app");
-const { getAnalytics } = require("firebase/analytics");
-const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBTd_9fw4vCPzFI0TcPrDtt3Gyhe11iL4w",
-    authDomain: "expert-57e13.firebaseapp.com",
-    projectId: "expert-57e13",
-    storageBucket: "expert-57e13.appspot.com",
-    messagingSenderId: "747965781429",
-    appId: "1:747965781429:web:b0e80cd41bdadced2eb5fc",
-    measurementId: "G-KV0YWQVCD2"
-};
-
-// Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebase);
-const db = getFirestore(firebase);
-
-async function getProducts(db) {
     const productsCol = collection(db, "products");
     const productsSnapshot = await getDocs(productsCol);
     const productsList = productsSnapshot.docs.map(doc => doc.data());
@@ -36,11 +12,9 @@ async function getProducts(db) {
     return productsList;
 }
 
-app.get("/", (req, res) => {
-    return res.send(getProducts(db))
+app.get("/", async (req, res) => {
+    return res.send(await getProducts())
 })
-
-
 
 app.listen(PORT, () => console.log("listening on PORT " + PORT))
 
