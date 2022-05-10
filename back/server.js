9c067b3c03061b2ea0d22757b3eeabe5723782b1
chaim malov <chaimalov@gmail.com>
2022-05-10 17:29:04 +0300

    server cleanup 2.0

--- back/server.js
+++ back/server.js
@@ -4,38 +4,40 @@ import { collection, getDocs, db, addDoc, query, where } from "./firebase.js";
 
 const app = express()
 const PORT = 8080;
-app.use(cors())
-app.use(express.json())
 
 const productsCol = collection(db, "products");
 
+app.use(cors())
+app.use(express.json())
+
 //Routes
+{
+    app.post("/add", async (req, res) => {
+        // Add a new document with a generated id.
+        const docRef = await addDoc(productsCol, {
+            name: req.body.name,
+            category: req.body.category
+        });
+    })
 
-app.post("/add", async (req, res) => {
-    // Add a new document with a generated id.
-    const docRef = await addDoc(productsCol, {
-        name: req.body.name,
-        category: req.body.category
-    });
-})
+    app.get("/search", async (req, res) => {
+        const searchRes = query(productsCol, where("name", "==", ""))
+        res.send((await getDocs(searchRes)).docs.map(doc => doc.data()))
+    })
 
-app.get("/search", async (req, res) => {
-    const searchRes = query(productsCol, where("name", "==", ""))
-    res.send((await getDocs(searchRes)).docs.map(doc => doc.data()))
-})
+    app.get("/", async (req, res) => {
+        return res.send(await getProducts())
+    })
 
-app.get("/", async (req, res) => {
-    return res.send(await getProducts())
-})
-
-app.listen(PORT, () => console.log("listening on PORT " + PORT))
-
+    app.listen(PORT, () => console.log("listening on PORT " + PORT))
+}
 
 //Functions
+{
+    async function getProducts() {
+        const productsSnapshot = await getDocs(productsCol);
+        const productsList = productsSnapshot.docs.map(doc => doc.data());
 
-async function getProducts() {
-    const productsSnapshot = await getDocs(productsCol);
-    const productsList = productsSnapshot.docs.map(doc => doc.data());
-
-    return productsList;
+        return productsList;
+    }
 }
