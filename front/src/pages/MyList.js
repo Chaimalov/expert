d6300ea641578899d7c5da884f28d17fc2ebd254
Chaim Malov <chaimalov@gmail.com>
2022-05-24 15:12:39 +0300

    loggedIn

--- front/src/pages/MyList.js
+++ front/src/pages/MyList.js
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { useAuth } from "../context/AuthContext"
-import { useProducts } from '../context/ProductsContext'
-import ProductsList from "../components/ProductsList"
-
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useProducts } from "../context/ProductsContext";
+import ProductsList from "../components/ProductsList";
 
 export default function MyList() {
-    const { user } = useAuth()
-    const { products } = useProducts()
-    const [items, setItems] = useState()
+  const { user, loggedIn } = useAuth();
+  const { products } = useProducts();
+  const [items, setItems] = useState();
 
-    useEffect(() => {
-        if (!user.uid || !products) return
-        setItems(products.filter(item => user.itemsArray.some(list => list == item.id)))
-    }, [user, products])
+  useEffect(() => {
+    if (!loggedIn) return;
+    setItems(
+      products.filter((item) => user.itemsArray.some((list) => list == item.id))
+    );
+  }, [user, products]);
 
-    if (items) return (
-        <ProductsList list={items} />
-    )
-    return <div></div>
+  if (items) return <ProductsList list={items} />;
+  return <div></div>;
 }
