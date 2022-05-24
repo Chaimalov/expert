2926880e48ea16aecddbf26482534686273a391a
Chaim Malov <chaimalov@gmail.com>
2022-05-24 15:11:23 +0300

    firebase back cleanup

--- back/firebase.js
+++ back/firebase.js
@@ -3,22 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection } from "firebase/firestore/lite";
 
-
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBTd_9fw4vCPzFI0TcPrDtt3Gyhe11iL4w",
-    authDomain: "expert-57e13.firebaseapp.com",
-    projectId: "expert-57e13",
-    storageBucket: "expert-57e13.appspot.com",
-    messagingSenderId: "747965781429",
-    appId: "1:747965781429:web:b0e80cd41bdadced2eb5fc",
-    measurementId: "G-KV0YWQVCD2"
+  apiKey: "AIzaSyBTd_9fw4vCPzFI0TcPrDtt3Gyhe11iL4w",
+  authDomain: "expert-57e13.firebaseapp.com",
+  projectId: "expert-57e13",
+  storageBucket: "expert-57e13.appspot.com",
+  messagingSenderId: "747965781429",
+  appId: "1:747965781429:web:b0e80cd41bdadced2eb5fc",
+  measurementId: "G-KV0YWQVCD2",
 };
 
 // Initialize Firebase
@@ -26,17 +23,7 @@ const firebase = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(firebase);
 const firestore = getFirestore(firebase);
 
-
-
-const db = {
-    products: collection(firestore, "products"),
-    users: collection(firestore, "users")
-}
-
-
-export { db }
-
-
-
-
-
+export const db = {
+  products: collection(firestore, "products"),
+  users: collection(firestore, "users"),
+};
