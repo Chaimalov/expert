9aa48d36115b24b2d29d5391f73bb3d5d5b2194f
chaim malov <chaimalov@gmail.com>
2022-05-25 20:31:40 +0300

    account transition

--- front/src/pages/Account.js
+++ front/src/pages/Account.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useAuth } from "../context/AuthContext"
-
+import Transitions from '../context/Transition'
 
 export function Account() {
 
     const { user } = useAuth()
     return (
-        <div className='center m2'>
-            <h1>Account</h1>
-            <img src={user?.photoURL} alt="profile" />
-            <h2>{user?.displayName}</h2>
-            <h2>{user?.email}</h2>
-        </div>
+        <Transitions>
+            <div className='center m2'>
+                <h1>Account</h1>
+                <img src={user?.photoURL} alt="profile" />
+                <h2>{user?.displayName}</h2>
+                <h2>{user?.email}</h2>
+            </div>
+        </Transitions>
     )
 }
