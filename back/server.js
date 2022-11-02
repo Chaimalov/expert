8811cfc4c264a998517981e9c79665a5a12cc02a
chaim malov <chaimalov@gmail.com>
2022-11-02 22:08:41 +0200

    fix(): server.js

--- back/server.js
+++ back/server.js
@@ -1,15 +1,14 @@
 import {} from "dotenv/config";
 import cors from "cors";
 import express from "express";
-import cors from "cors";
-import { searchByCategory, searchByName } from "./searchEmoji.js";
+import productsController from "./controllers/productsController";
+import userController from "./controllers/userController";
 
 const app = express();
 const PORT = 8080;
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/products", productsRoute);
-app.use("/users", usersRoute);
-
-app.listen(PORT, () => console.log("listening on PORT " + PORT));
+app.use("/products", productsController);
+app.use("/users", userController);
