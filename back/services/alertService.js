cd925f75e713fb9f17bf6995a125e58de43347ba
israelmark1 <israelmark98@gmail.com>
2022-11-03 10:22:21 +0200

    implement alertService

--- back/services/alertService.js
+++ back/services/alertService.js
@@ -2,12 +2,29 @@ import { getProductsByUser, addDaysToDate } from "./productsService";
 import { getAllUsers } from "./userService";
 import { sendEmail } from "../sendMassage";
 
+let allUsers = getAllUsers();
+
 const getExpiredProducts = (userId, notifyBefore) => {
   const expiredProducts = getProductsByUser(userId).filter((product) => {
     return product.expiryDate <= addDaysToDate(new Date(), -notifyBefore);
   });
   return expiredProducts;
 };
+
 const sendEmailToUser = (user) => {
-  sendEmail(user.email, "", "");
+  const products = getExpiredProducts(user, user.notifyBefore);
+  let subject = `your products are getting expired!`;
+  let message = `products: ${products}`;
+
+  sendEmail(user.email, subject, message);
+};
+
+const sendEmailToExpired = () => {
+  allUsers.forEach((user) => {
+    sendEmailToUser(user);
+  });
+};
+
+export default {
+  sendEmailToExpired,
 };
