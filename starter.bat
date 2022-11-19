d7c0ad7ce643ed4905b2abee036c3a26ddae3428
chaim malov <chaimalov@gmail.com>
2022-11-20 00:40:46 +0200

    feat(): added starter.bat script

---
+++ starter.bat
@@ -0,0 +1,15 @@
+
+:: Start server
+cd ex.server
+start cmd.exe /k "npm start"
+
+:: Start client
+cd ../ex.client
+start cmd.exe /k "npm start"
+
+:: Start emoji API
+cd ../ex.emoji
+start cmd.exe /k "npm start"
+
+:: Go back
+cd ..
