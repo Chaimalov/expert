429a5654c23cc766fbfe369cfdf4852b374f9d18
israelmark <israelmark98@gmail.com>
2024-06-03 22:11:41 +0300

    fix(): deleting dist from the repo

--- ex.emoji/dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import emojiController from "./controller.js";
-const app = express();
-const PORT = 9090;
-app.use(cors());
-app.use(express.json());
-app.use("/emojis", emojiController);
-app.listen(PORT, () => console.log("listening on PORT " + PORT));
